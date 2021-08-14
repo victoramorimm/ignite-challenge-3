75a93f7de25e30d9cf24186708ed791d4a34ead3
Victor Amorim <victorvmrgamer@gmail.com>
2021-08-14 11:31:38 -0300

    feat: create findByTitleContaining method

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -1,9 +1,9 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from "typeorm";
 
-import { User } from '../../../users/entities/User';
-import { Game } from '../../entities/Game';
+import { User } from "../../../users/entities/User";
+import { Game } from "../../entities/Game";
 
-import { IGamesRepository } from '../IGamesRepository';
+import { IGamesRepository } from "../IGamesRepository";
 
 export class GamesRepository implements IGamesRepository {
   private repository: Repository<Game>;
@@ -11,20 +11,28 @@ export class GamesRepository implements IGamesRepository {
   constructor() {
     this.repository = getRepository(Game);
   }
+  countAllGames(): Promise<[{ count: string }]> {
+    throw new Error("Method not implemented.");
+  }
+  findUsersByGameId(id: string): Promise<User[]> {
+    throw new Error("Method not implemented.");
+  }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    console.log(param);
+
+    return await this.repository
+      .createQueryBuilder("game")
+      .where("game.title ILIKE :title", { title: `%${param}%` })
+      .getMany();
   }
 
-  async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
-  }
+  // async countAllGames(): Promise<[{ count: string }]> {
+  //   return this.repository.query(); // Complete usando raw query
+  // }
 
-  async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
-  }
+  // async findUsersByGameId(id: string): Promise<User[]> {
+  //   return this.repository.createQueryBuilder();
+  //   // Complete usando query builder
+  // }
 }
